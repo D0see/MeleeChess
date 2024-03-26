211c629c9b1953bad2407bec0edfce9c4c98903e
D0see <317Y17@gmail.com>
2024-03-26 01:01:50 +0100

    built characterselectscreen sliding up animation logic

--- src/App.js
+++ src/App.js
@@ -2,20 +2,31 @@ import './App.css';
 import CharacterSelectScreen from './containers/TeamCompScreen/CharacterSelectScreen.js';
 import React, {useState, useEffect} from 'react';
 
-
+// ? Rename App.js CharacterSelectScreenApp.js, create a new App.js container containing this mess, pass it the useState for the teams, prop drill one layer further with it.
+// Separate every other modules in other apps.
 function App() {
   const [teamsState, setTeamsState] = useState({"White": null, "Black": null});
   const [playerTeams, setPlayerTeams] = useState({"White": null, "Black": null});
 
-  if(teamsState["Black"] && teamsState["White"]) {
-    return (
-      <p>{JSON.stringify(playerTeams)}</p>
-    )
-  } else {
-    return (
-      <CharacterSelectScreen setPlayerTeams={setPlayerTeams} setTeamsState={setTeamsState}/>
-    );
-  }
+  const [className, setClassName] = useState("");
+  const [CharacterSelectfinished, setCharacterSelectfinished] = useState(false);
+
+  useEffect(() => {
+    if(teamsState["Black"] && teamsState["White"]) {
+      setClassName("Slide-up");
+      const timeOut = setTimeout(() => {
+        setCharacterSelectfinished(true)
+      }, 3500)
+      return () => clearTimeout(timeOut);
+    }
+  }, [teamsState])
+//Line 27 is just for testing purposes
+  return (
+    <>
+      {!CharacterSelectfinished && <CharacterSelectScreen className={className} setPlayerTeams={setPlayerTeams} setTeamsState={setTeamsState}/>} 
+      {CharacterSelectfinished && <p>{JSON.stringify(playerTeams)}</p>}
+    </>
+  )
 }
 
 export default App;
--- src/containers/TeamCompScreen/CharacterSelectScreen.js
+++ src/containers/TeamCompScreen/CharacterSelectScreen.js
@@ -1,11 +1,11 @@
-import React, {useState} from 'react';
+import React from 'react';
 import PlayerTeamComposition from './PlayerTeamComposition';
 import './CharacterSelectScreen.css';
 
-export default function CharacterSelectScreen({setPlayerTeams, setTeamsState}) {
+export default function CharacterSelectScreen({setPlayerTeams, setTeamsState, className}) {
 
   return (
-    <div className='CharacterSelectScreen'>
+    <div className={`${className} CharacterSelectScreen`}>
         <PlayerTeamComposition playerColor={"White"} setPlayerTeams={setPlayerTeams} setTeamsState={setTeamsState}/>
         <PlayerTeamComposition playerColor={"Black"} setPlayerTeams={setPlayerTeams} setTeamsState={setTeamsState}/>
     </div>
